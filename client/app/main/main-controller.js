bb4f70708df6130679f0ee0dded479420bde08b1
Allen Vestal <allen@godaddy.com>
2017-04-12 16:43:06 -0700

    update service
    update service

--- client/app/main/main-controller.js
+++ client/app/main/main-controller.js
@@ -4,12 +4,14 @@ angular.module('app')
     $scope.list1 = [
       {label: 'one'},
       {label: 'two'},
-      {label: 'three'}
+      {label: 'three'},
+      {label: 'four'}
     ];
 
     $scope.list2 = [
       {label: 'uno'},
       {label: 'dos'},
-      {label: 'tres'}
+      {label: 'tres'},
+      {label: 'quatro'}
     ];
   }]);
